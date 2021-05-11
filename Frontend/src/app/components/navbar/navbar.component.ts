15ed8bc79db0787414f6b2c1e99e7f90713efb84
vijaropin <vijaro-4@hotmail.com>
2021-05-10 21:15:02 -0500

    agregar alert a buscar

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -12,4 +13,16 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
+  BR(){
+    Swal.fire({
+      title:'¡Proximamente!',
+      text:'¡¡¡ Ahora estas en la versión beta, Posteriormente puedes buscar tu Tienda o negocio !!!',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+
+}}
